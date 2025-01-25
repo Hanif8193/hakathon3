949596dcdc1100ba8877594e76a4f66d7df9b4c5
Hanif8193 <hanifdostmuhammad8193@gmail.com>
2025-01-25 11:27:42 +0500

    third commit

--- src/app/product/[slug]/page.tsx
+++ src/app/product/[slug]/page.tsx
@@ -1,11 +1,10 @@
 "use client";
-
 import { client } from "@/sanity/lib/client";
 import { groq } from "next-sanity";
 import Image from "next/image";
 import { urlFor } from "@/sanity/lib/image";
 import React, { useState, useEffect } from "react";
-
+import { Button } from "@/components/ui/button";
 
 
 interface IProduct {
@@ -39,9 +38,17 @@ async function getProduct(slug: string): Promise<IProduct | null> {
   );
 }
 
+
 export default function ProductPage({ params }: ProductPageProps) {
   const { slug } = params;
   const [product, setProduct] = useState<IProduct | null>(null);
+  const [count,setCount] = useState(0);
+  function increment () {
+    setCount(count + 1);
+  }
+ function decrement  () {
+  setCount(count - 1);
+ }
 
   useEffect(() => {
     async function fetchProduct() {
@@ -57,6 +64,8 @@ export default function ProductPage({ params }: ProductPageProps) {
   if (!product) {
     return <p>Product not found.</p>;
   }
+ 
+  
 
   return (
     <div className="max-w-10xl mx-auto p-4">
@@ -70,6 +79,13 @@ export default function ProductPage({ params }: ProductPageProps) {
         <div className="flex flex-col gap-5 items-center ml-5">
           <h1 className="text-2xl font-bold">{product.name}</h1>
           <p className="text-2xl font-bold">${product.price}</p>
+          <div className='flex justify-center items-center h-screen gap-5'>
+    <Button onClick={increment} variant={"default"} className='text-2xl rounded-3xl py-6'>Increment</Button>
+     <Button onClick={decrement}variant={"destructive"} className='text-2xl rounded-3xl py-6'>Decrement</Button>
+          
+     <p> count= {count} </p> 
+    
+    </div>
           <p>{product.description}</p>
           <button
             className="bg-black text-white px-10 py-2 rounded-lg hover:cursor-pointer"
@@ -82,3 +98,4 @@ export default function ProductPage({ params }: ProductPageProps) {
     </div>
   );
 }
+
